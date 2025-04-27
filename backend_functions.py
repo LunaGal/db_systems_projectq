f4b8dd0f9181befdcd237c2bc6096a3a9577e830
Luna Gal <lunagal@Lunas-MacBook-Air.local>
2025-04-27 15:51:12 -0400

    added some backend functions

---
+++ backend_functions.py
@@ -0,0 +1,33 @@
+# Create
+
+def makeUser(username, password):
+    return None
+
+
+# Update
+
+def setStarred(username, recipe, starred):
+    return None
+
+def addPlannedMeal(username, recipe, servings):
+    return None
+
+def changeServings(username, recipe, newServings):
+    return None
+
+def removePlannedMeal(username, recipe):
+    return None
+
+# Retrieve
+
+# returns true if password matches password of user, false otherwise
+def isPassword(user, password):
+    return None
+
+def getMealPlan(user):
+    return None
+
+# Delete
+
+def deleteUser(username):
+    return None
